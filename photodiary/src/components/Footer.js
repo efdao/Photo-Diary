fe8b463f9bc0abaea2711fc59867f6fa40319b2b
efdao <150654090+efdao@users.noreply.github.com>
2024-06-11 11:52:05 +0900

    Update Footer.js

--- photodiary/src/components/Footer.js
+++ photodiary/src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
   return(
       <div className={styles.Footer}>
         <p>© 2024 김동현&황지현 All rights reserved</p>
-        <a href="http://github.com">
+        <a href="https://github.com/efdao/Photo-Diary" target="_blank" rel="noopener noreferrer">
           <img src={footer} alt="footer"/>
         </a>
       </div>
